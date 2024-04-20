8696806aaf491f978480c0094db371f7749062fa
Said <alhendisaid@gmail.com>
2024-04-20 08:32:59 +0200

    updata earth

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,14 +1,32 @@
 import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import { OrbitControls, Preload, meshBounds, useGLTF } from '@react-three/drei'
 
 import CanvasLoader from '../Loader'
 
 const Earth = () => {
-  const earth = useGLTF('./planet/scene.gltf')
+  const earth = useGLTF('./planet2/scene.gltf')
 
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    <>
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1} />
+      <hemisphereLight intensity={0.15} groundColor="black" />
+
+      <primitive
+        object={earth.scene}
+        scale={1.5}
+        position={[0, 0, 0]}
+        rotation={[-10, 4, 2]}
+      />
+    </>
   )
 }
 
@@ -16,12 +34,12 @@ const EarthCanvas = () => {
   return (
     <Canvas
       shadows
-      frameloop='demand'
+      frameloop="demand"
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
       camera={{
-        fov: 45,
-        near: 0.1,
+        fov: 80,
+        near: 2,
         far: 200,
         position: [-4, 3, 6],
       }}
